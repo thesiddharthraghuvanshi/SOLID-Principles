ae2f40aeb74f308605c6c797fd2e6a2651af19f8
thesiddharthraghuvanshi <97022289+thesiddharthraghuvanshi@users.noreply.github.com>
2024-01-11 19:15:31 +0000

    updated Liskov substitution principle

--- LiskovSubstitutionPrinciple/Child.java
+++ LiskovSubstitutionPrinciple/Child.java
@@ -10,4 +10,11 @@ public class Child extends Parent {
 
   //this princple says that child should always include the property of it's parent and add it's own
   //child should never exclude the parent property, in here it is walk()
+
+  /*@Override
+  public void walk() {
+    System.out.println("Child is too small to walk");
+  }*/
+  //in this case you should create a different class for walking and only parent will extend it and child will not
+  
 }
